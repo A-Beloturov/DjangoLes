c38b53aa1d1f15e22afcaddb3f85b466322e6eae
A-Beloturov <a.beloturov@bk.ru>
2023-08-05 12:50:08 +0400

    Add migrations

---
+++ djless/main/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Address(models.Model):
+    address = models.TextField()
+
+
+class Users(models.Model):
+    user_name = models.CharField(max_length=20)
+    user_surname = models.CharField(max_length=20)
+    user_mail = models.CharField(max_length=20)
+    user_address = models.ForeignKey(Address, on_delete=models.PROTECT)
+
+# Create your models here.
